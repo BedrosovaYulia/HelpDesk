e7d06b04fabdaa47bef6dddb544df92d83ca0891
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-06-01 10:40:47 +0300

    Update result_modifier.php

--- bitrix/templates/bitrix24/components/bitrix/support.ticket/bedrosova/bedrosova/support.ticket.list/bedrosova/result_modifier.php
+++ bitrix/templates/bitrix24/components/bitrix/support.ticket/bedrosova/bedrosova/support.ticket.list/bedrosova/result_modifier.php
@@ -1,6 +1,6 @@
 <?
-
-
+global $USER;
+$CurrentUser=$USER->GetLogin();
 
 $TicketTaskMap=array();
 $TicketRespMap=array();
@@ -20,6 +20,22 @@ while($ob = $res->GetNextElement()){
 
 foreach($arResult['ROWS'] as $key=>$row){
 	//$arResult['ROWS'][$key]['data']['OWNER_SID']='';
+	//print $row['data']['RESPONSIBLE_LOGIN'];
+	//print $row['data']['LAST_MESSAGE_BY_SUPPORT_TEAM'];
+	
+	if ($row['data']['RESPONSIBLE_LOGIN']==$CurrentUser && $row['data']['LAST_MESSAGE_SID']==$row['data']['OWNER_SID']){
+		$arResult['ROWS'][$key]['data']['LAMP']="red";
+		$arResult['ROWS'][$key]['data']['~LAMP']="red";
+		$arResult['ROWS'][$key]['columns']['LAMP']='<div class="support-lamp-red" title=" last posted by a techsupport client (you are responsible)"></div>';
+		//$arResult['ROWS'][$key]['columns']['LAMP']='<div class="support-lamp-yellow" title=" last posted by a techsupport client (you are not responsible)"></div>';	
+	}
+	
+	if ($row['data']['RESPONSIBLE_LOGIN']!=$CurrentUser && $row['data']['LAST_MESSAGE_SID']==$row['data']['OWNER_SID']){
+		$arResult['ROWS'][$key]['data']['LAMP']="yellow";
+		$arResult['ROWS'][$key]['data']['~LAMP']="yellow";
+		//$arResult['ROWS'][$key]['columns']['LAMP']='<div class="support-lamp-red" title=" last posted by a techsupport client (you are responsible)"></div>';
+		$arResult['ROWS'][$key]['columns']['LAMP']='<div class="support-lamp-yellow" title=" last posted by a techsupport client (you are not responsible)"></div>';	
+	}
 
 	if (!empty($TicketTaskMap)){
 		$strTask='';
@@ -30,7 +46,7 @@ foreach($arResult['ROWS'] as $key=>$row){
 			$strCont='<a href="/crm/contact/details/'.$TicketContMap[$row['data']['ID']].'/">Contact>></a>';
 
 		$arResult['ROWS'][$key]['data']['OWNER_SID'].="<br/>".$strTask." ".$strCont;
-	}
+	}		
 
 }
 
